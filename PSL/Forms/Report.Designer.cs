2dbadbb50767c5c8ee4b8118c56fd5b74e2e867f
Thinh Ha <havanthinh97@gmail.com>
2022-05-04 21:50:45 +0700

    Add Report Form.
    Install ReportViewer and add Report Form.

---
+++ PSL/Forms/Report.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PSL.Forms
+{
+    partial class Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rvMain = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rvMain);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1902, 1033);
+            this.panel1.TabIndex = 0;
+            // 
+            // rvMain
+            // 
+            this.rvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rvMain.Location = new System.Drawing.Point(0, 0);
+            this.rvMain.Name = "rvMain";
+            this.rvMain.ServerReport.BearerToken = null;
+            this.rvMain.Size = new System.Drawing.Size(1902, 1033);
+            this.rvMain.TabIndex = 0;
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.panel1);
+            this.Name = "Report";
+            this.Text = "Report";
+            this.Load += new System.EventHandler(this.Report_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.Reporting.WinForms.ReportViewer rvMain;
+    }
+}
